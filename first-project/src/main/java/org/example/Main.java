474af0efff282baad6d4a88bd8fe5e7fad6e3a62
ErOpg-oss <kutcepaloverop@gmail.com>
2025-12-04 11:31:59 +0300

    finding max

--- first-project/src/main/java/org/example/Main.java
+++ first-project/src/main/java/org/example/Main.java
@@ -30,4 +30,18 @@ public class Main {
         System.out.println(human.getAge());
 
     }
+
+    public static int findMax(int a, int b, int c, int d) {
+        int max = a;
+        if (b > max) {
+            max = b;
+        }
+        if (c > max) {
+            max = c;
+        }
+        if (d > max) {
+            max = d;
+        }
+        return max;
+    }
 }
